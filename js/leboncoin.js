7d803e37ad7172bb8d33b2ea4d3a1176dd5a3ef5
Natiassa <natacha.guillet@devinci.fr>
2017-02-08 14:32:32 +0100

    Modules are organized correctly

--- js/leboncoin.js
+++ js/leboncoin.js
@@ -1,47 +1,40 @@
-var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var app     = express();
-
 var json = {
- "prix" : 0,
- "codePostal": 0,
- "ville":"",
- "type":"",
- "surface":""
+  "prix" : 0,
+  "codePostal": 0,
+  "ville":"",
+  "type":"",
+  "surface":""
 };
 
-app.get('/scrape', function(req,res){
-  url= 'https://www.leboncoin.fr/ventes_immobilieres/1086051198.htm?ca=12_s';
+var readLeboncoin = function(fs,url){
   request(url, function(error, response, html){
     // First we'll check to make sure no errors occurred when making the request
 
     if(!error){
       var $ = cheerio.load(html);
       $('#adview > section > section > section.properties.lineNegative > div:nth-child(9) > h2 > span.value').filter(function(){
-          var data = $(this);
+        var data = $(this);
         json.surface = data.text().replace(/[^0-9]+/ig,"").slice(0,json.surface.length-1);
       })
       $('#adview > section > section > section.properties.lineNegative > div:nth-child(5) > h2 > span.value').filter(function(){
-          var data = $(this);
+        var data = $(this);
         json.prix = data.text().replace(/[^0-9]+/ig,"");
       })
       $('#adview > section > section > section.properties.lineNegative > div.line.line_city > h2 > span.value').filter(function(){
-          var data = $(this);
+        var data = $(this);
         json.codePostal = data.text().replace(/[^0-9]+/ig,"");
         json.ville = data.text().replace(new RegExp("[^(a-zA-Z)\-]", "g"), '');
       })
       $('#adview > section > section > section.properties.lineNegative > div:nth-child(7) > h2 > span.value').filter(function(){
-          var data = $(this);
+        var data = $(this);
         json.type = data.text();
         if(json.type === "Oui" || json.type === "Non"){
           $('#adview > section > section > section > div:nth-child(8) > h2 > span.value').filter(function(){
-              var data = $(this);
+            var data = $(this);
             json.type = data.text();
           })
           $('#adview > section > section > section > div:nth-child(10) > h2 > span.value').filter(function(){
-              var data = $(this);
+            var data = $(this);
             json.surface = data.text().replace(/[^0-9]+/ig,"").slice(0,json.surface.length-1);
           })
         }
@@ -51,12 +44,6 @@ app.get('/scrape', function(req,res){
       console.log('File successfully written! - Check your project directory for the output.json file');
 
     })
-    res.send('Check your console!')
   });
-})
-
-
-
-app.listen('8081')
-console.log('Magic happens on port 8081');
-exports = module.exports = app;
+}
+exports.readLeboncoin = readLeboncoin;
--- js/meilleuragents.js
+++ js/meilleuragents.js
@@ -1,67 +1,80 @@
-var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var app     = express();
-
 var info = {
   "prixsquare":0,
   "prixbas":0,
-  "prixmoyen":"",
-  "prixhaut":0
+  "prixmoyen":0,
+  "prixhaut":0,
+  "type":""
 };
 
-var data = JSON.parse(fs.readFileSync("output.json","UTF-8"));
-app.get('/scrape2',function(req,res){
-      url= 'https://www.meilleursagents.com/prix-immobilier/'+ data.ville.toLowerCase() + '-'+data.codePostal;
-      request(url, function(error, response, html){
-        if(!error){
-          var $ = cheerio.load(html);
-          if(data.type === "Maison"){
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
-              var price = $(this);
-              info.prixmoyen = price.text().replace(/[^0-9]+/ig,"");
-            })
-            /*$('#synthese > div.prices-summary.baseline > div.prices-summary__values > div.row.medium-uncollapse.baseline--half > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
-              var price = $(this);
-              info.prixmoyen = price.text().replace(/[^0-9]+/ig,"");
-            })*/
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').filter(function(){
-              var price = $(this);
-              info.prixbas = price.text().replace(/[^0-9]+/ig,"");
-            })
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div:nth-child(4)').filter(function(){
-              var price = $(this);
-              info.prixhaut = price.text().replace(/[^0-9]+/ig,"");
-            })
-          }
-          else if(data.type ==="Appartement"){
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
-              var price = $(this);
-              info.prixmoyen = price.text().replace(/[^0-9]+/ig,"");
-            })
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').filter(function(){
-              var price = $(this);
-              info.prixbas = price.text().replace(/[^0-9]+/ig,"");
-            })
-            $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div:nth-child(4)').filter(function(){
-              var price = $(this);
-              info.prixhaut = price.text().replace(/[^0-9]+/ig,"");
-            })
-          }
-        }
-        info.prixsquare = data.prix / data.surface ;
-        console.log(info.prixsquare);
-        console.log(info.prixmoyen);
-        console.log(url);
-        if(info.prixsquare > info.prixmoyen){
-          res.send("Mauvais deal");
-        } else {
-          res.send("Good deal");
-        }
-      });
-})
 
-app.listen('8081')
-console.log('Magic happens on port 8081');
-exports = module.exports = app;
+var priceSquareMeter = function(fs){
+  var data = JSON.parse(fs.readFileSync("output.json","UTF-8"));
+  var url= 'https://www.meilleursagents.com/prix-immobilier/'+ data.ville.toLowerCase() + '-'+data.codePostal;
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+      $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div.row.medium-uncollapse.baseline--half > div.small-12.medium-6.columns.prices-summary__cell--row-header').filter(function(){
+        var price = $(this);
+        info.type = price.text();
+        console.log(info.type.includes("maison"));
+      })
+      if(data.type === "Maison"){
+        if (info.type.includes("maison")) {
+          $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div.row.medium-uncollapse.baseline--half > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
+            var price = $(this);
+            info.prixmoyen = price.text().replace(/[^0-9]+/ig,"");
+          })
+          $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div.row.medium-uncollapse.baseline--half > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').filter(function(){
+            var price = $(this);
+            info.prixbas = price.text().replace(/[^0-9]+/ig,"");
+          })
+          $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div.row.medium-uncollapse.baseline--half > div:nth-child(4)').filter(function(){
+            var price = $(this);
+            info.prixhaut = price.text().replace(/[^0-9]+/ig,"");
+          })
+        } else {
+          $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
+            var price = $(this);
+            info.prixmoyen = price.text().replace(/[^0-9]+/ig,"");
+          })
+          $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').filter(function(){
+            var price = $(this);
+            info.prixbas = price.text().replace(/[^0-9]+/ig,"");
+          })
+          $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(3) > div:nth-child(4)').filter(function(){
+            var price = $(this);
+            info.prixhaut = price.text().replace(/[^0-9]+/ig,"");
+          })
+        }
+      }
+      else if(data.type ==="Appartement"){
+        $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.columns.prices-summary__cell--median').filter(function(){
+          var price = $(this);
+          info.prixmoyen = price.text().replace(/[^0-9]+/ig,"");
+        })
+        $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div.small-4.medium-2.medium-offset-0.columns.prices-summary__cell--muted').filter(function(){
+          var price = $(this);
+          info.prixbas = price.text().replace(/[^0-9]+/ig,"");
+        })
+        $('#synthese > div.prices-summary.baseline > div.prices-summary__values > div:nth-child(2) > div:nth-child(4)').filter(function(){
+          var price = $(this);
+          info.prixhaut = price.text().replace(/[^0-9]+/ig,"");
+        })
+      }
+    }
+    info.prixsquare = data.prix / data.surface ;
+    console.log(info.prixsquare);
+    console.log(info.prixmoyen);
+  });
+}
+
+var isGoodDeal = function(){
+  if(info.prixsquare > info.prixmoyen){
+    console.log("Mauvais deal");
+  } else {
+    console.log("Good deal");
+  }
+}
+
+exports.isGoodDeal = isGoodDeal;
+esports.priceSquareMeter = priceSquareMeter;
